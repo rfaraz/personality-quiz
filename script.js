58f9e4a3d46013a4015b11c4ad8127d4765d2ffe
Rida <63632471+rfaraz@users.noreply.github.com>
2020-08-05 17:20:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//Global Variables
+//Initiate variables to track the questions answered and the score for each answer choice
+var answered = 0;
+var fireScore = 0;
+var waterScore = 0;
+var airScore = 0;
+var earthScore = 0;
+
+
+
+
+
+//Initiate variables to store all the buttons
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
+var q6a1 = document.getElementById("q6a1");
+var q6a2 = document.getElementById("q6a2");
+var q6a3 = document.getElementById("q6a3");
+var q6a4 = document.getElementById("q6a4");
+
+//#TODO: Add Event Listeners to your answer choice variables.
+q1a1.addEventListener("click", result1);
+
+
+
+
+
+
+
+//#TODO: Define quiz functions here
+function result1(){
+  
+  
+  
+}
